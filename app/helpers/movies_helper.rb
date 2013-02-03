87fe0a65775ce370483e300994698a54c5facc2e
Magdalena Hosiawa <mhosiawa@gmail.com>
2013-02-03 15:41:12 +0100

    refactored movies index view

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,13 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+
+  def link_to_movies_title
+  	link_to "Movie Title", movies_path(:order_by=>:title), :id=>:title_header
+  end
+
+  def link_to_movies_release_date
+  	link_to "Release Date", movies_path(:order_by=>:release_date), :id=>:release_date_header
+  end
+
 end
