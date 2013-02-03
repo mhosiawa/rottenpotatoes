2a37845dc3587f31494fed9ca2887f01ae7b4acc
Magdalena Hosiawa <mhosiawa@gmail.com>
2013-02-03 16:28:49 +0100

    filtr working added

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -5,11 +5,11 @@ module MoviesHelper
   end
 
   def link_to_movies_title
-  	link_to "Movie Title", movies_path(:order_by=>:title), :id=>:title_header
+  	link_to "Movie Title", movies_path(:order_by=>:title, :ratings=>params[:ratings]), :id=>:title_header
   end
 
   def link_to_movies_release_date
-  	link_to "Release Date", movies_path(:order_by=>:release_date), :id=>:release_date_header
+  	link_to "Release Date", movies_path(:order_by=>:release_date, :ratings=>params[:ratings]), :id=>:release_date_header
   end
 
 end
